1cb4ecfd3fcd55971260f33b253f2b7a0ac80e47
Noman Rafiq <heyimnomee@gmail.com>
2025-05-04 13:18:38 +0500

    django fresh start

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('#hamburger');
-const hamMenu = document.querySelector('#menu');
-
-hamburger.addEventListener('click', () => {
-    hamMenu.classList.toggle('hidden');
-    hamburger.classList.toggle('bg-white');
-});
